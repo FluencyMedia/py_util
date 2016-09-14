8fbb7e4b1634ff04b30bd9000f94966e2247b703
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-09-13 22:12:29 -0400

    Added central 'config_main.py'

---
+++ config_main.py
@@ -0,0 +1,10 @@
+from time import strftime
+
+#%% Global variables
+
+strTimestamp = strftime("%m/%d/%Y %H:%M%p")
+
+#%%
+
+if __name__ == "__main__":
+    print("ERROR: Just tried to run 'config_main.py' as main module")
